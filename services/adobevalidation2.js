622bb97345759e281d2285f2503b0e7a1bf03ea0
taha-khan-rws <taha.sait@outlook.com>
2025-05-21 23:32:37 +0530

    optimizing runtime:=>, effor:=>1

--- services/adobevalidation2.js
+++ services/adobevalidation2.js
@@ -1,17 +1,40 @@
 const puppeteer = require("puppeteer");
 
-const validateAdobeBeacons2 = async (urls) => {
-    urls = urls[0,20]
-  const browser = await puppeteer.launch({ headless: true });
-
+const validateAdobeBeacons2 = async () => {
+  const browser = await puppeteer.launch({ timeout: 0 });
+  let urls = [];
   const page = await browser.newPage();
+
+  await page.goto("https://www.channelnewsasia.com/", {
+    waitUntil: "load",
+    timeout: 0,
+  });
+
+  const links = await page.evaluate(() => {
+    const anchorTags = document.querySelectorAll("a");
+    const filteredURL = new Set();
+
+    anchorTags.forEach((anchor) => {
+      const href = anchor.href;
+      if (href.startsWith("https://www.channelnewsasia.com/")) {
+        filteredURL.add(href);
+      }
+    });
+
+    return Array.from(filteredURL);
+  });
+  // urls = urls.slice(0, 21);
+  urls = links.slice(0, 21);
+  console.log("List of urls ", urls);
+
   try {
-    for (const i in urls) {
-      //   console.log(`Navigating to: ${urls[i]}`);
-      await page.goto(urls[i], { waitUntil: "load", timeout: 0 }); // waitUntil : 'networkidle2'
+    for (let url of urls) {
+      console.log(`Navigating to: ${url}`);
+      await page.goto(url, { waitUntil: "load", timeout: 0 }); // waitUntil : 'networkidle2'
+      await page.setRequestInterception(true);
 
       // Check for Adobe Analytics tag presence in scripts
-      //   console.log(`Validating Adobe Analytics tag presence on: ${urls[i]}`);
+      //   console.log(`Validating Adobe Analytics tag presence on: ${url}`);
       const hasAdobeAnalyticsTag = await page.evaluate(() => {
         const scripts = Array.from(document.querySelectorAll("script"));
         return scripts.some(
@@ -27,25 +50,27 @@ const validateAdobeBeacons2 = async (urls) => {
       }
 
       // Monitor network requests for Adobe Analytics requests
-      //   console.log(`Monitoring network requests on: ${urls[i]}`);
+      //   console.log(`Monitoring network requests on: ${url}`);
 
       page.on("request", (request) => {
+        console.log("requested urls ::", request.url());
         if (request.url().includes("/b/ss/")) {
           // Adobe Analytics request endpoint
-
-          // console.log('✔ Adobe Analytics request sent');
-        }
-        // else {
-        //
-        // }
-      });
-
-      page.on("response", async (response) => {
-        if (response.url().includes("/b/ss/")) {
-          if (response.status == 200) {
-          }
+          request.continue();
+          console.log("✔ Adobe Analytics request sent");
+        } else {
+          request.abort();
         }
       });
+
+      // # commenting response , will look later about significance of validating reposnse
+      // page.on("response", async (response) => {
+      //   if (response.url().includes("/b/ss/")) {
+      //     if (response.status == 200) {
+      //       console.log("adobe analytics request response received");
+      //     }
+      //   }
+      // });
 
       // Validate Adobe Data Layer presence
       // console.log(`Validating Adobe Data Layer on: ${url}`);
@@ -67,6 +92,65 @@ const validateAdobeBeacons2 = async (urls) => {
   await page.close();
   await browser.close();
   // }
-  return {};
+  // return {};
 };
-module.exports = validateAdobeBeacons2;
+// module.exports = validateAdobeBeacons2;
+validateAdobeBeacons2();
+
+// urls.forEach(async (url) => {
+//   {
+//     console.log(`Navigating to: ${url}`);
+//     await page.goto(url, { waitUntil: "load", timeout: 0 }); // waitUntil : 'networkidle2'
+
+//     // Check for Adobe Analytics tag presence in scripts
+//     //   console.log(`Validating Adobe Analytics tag presence on: ${url}`);
+//     const hasAdobeAnalyticsTag = await page.evaluate(() => {
+//       const scripts = Array.from(document.querySelectorAll("script"));
+//       return scripts.some(
+//         (script) =>
+//           script.innerHTML.includes("s.t") ||
+//           script.innerHTML.includes("s.tl")
+//       );
+//     });
+//     if (hasAdobeAnalyticsTag) {
+//       // console.log('✔ Adobe Analytics tag found');
+//     } else {
+//       // console.log('✘ Adobe Analytics tag NOT found');
+//     }
+
+//     // Monitor network requests for Adobe Analytics requests
+//     //   console.log(`Monitoring network requests on: ${url}`);
+
+//     page.on("request", (request) => {
+//       console.log("requested urls ::", request.url());
+//       if (request.url().includes("/b/ss/")) {
+//         // Adobe Analytics request endpoint
+//         console.log("✔ Adobe Analytics request sent");
+//       }
+//       // else {
+//       //
+//       // }
+//     });
+
+//     page.on("response", async (response) => {
+//       if (response.url().includes("/b/ss/")) {
+//         if (response.status == 200) {
+//           console.log("adobe analytics request response received");
+//         }
+//       }
+//     });
+
+//     // Validate Adobe Data Layer presence
+//     // console.log(`Validating Adobe Data Layer on: ${url}`);
+//     const hasValidAdobeDataLayer = await page.evaluate(() => {
+//       return !!window.dataLayer && window.dataLayer.length > 0; // Checks if adobeDataLayer object exists
+//     });
+//     if (hasValidAdobeDataLayer) {
+//       // console.log('✔ Valid Adobe Data Layer present');
+//     } else {
+//       // console.log('✘ Adobe Data Layer NOT found or invalid');
+//     }
+
+//     // Close the page
+//   }
+// });
